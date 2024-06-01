476c19c8fc38f04a75ccb60921f5e9dc6a4eb113
Даниил <dgandapas1@gmail.com>
2024-06-01 09:04:52 +0300

    commit

---
+++ Infrastructure/Jobs/TestJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace Infrastructure1.Jobs;
+
+public class TestJob: IJob
+{
+    public Task Execute(IJobExecuteContext context)
+    {
+        Console.WriteLine();
+        return Task.CompletedTask;
+    }
+}
--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -3,7 +3,9 @@ using Application.Mapping;
 using Application.Services;
 using Infrastructure.Dal.EntityFramework;
 using Infrastructure.Dal.Repositories;
+using Infrastructure1.Jobs;
 using Microsoft.EntityFrameworkCore;
+using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 // Добавление настроек из файла.
@@ -17,6 +19,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<PersonService>();
+///TODO: типизировать блок CronExpression
+var testJobCronExpression = builder.Configuration["CronExpression:TestJob"];
+
+
+builder.Services.AddQuartz(x =>
+{
+    /// найти современный вариань
+    x.UseMicrosoftDependencyInjectionFactory();
+
+    var jobKey = new JobKey("TestJob");
+
+    x.AddJob<TestJob>(opts => opts.WithIdentity(jobKey));
+
+    var triggerKey = new TriggerKey("TestJobTrigger");
+
+    x.AddTrigger(opts => opts.ForJob(jobKey).WithIdentity(triggerKey)
+        .WithCronSchedule(testJobCronExpression));
+});
+///TODO: Read about Cron, добавить cron запись в appsettings
+
+
+builder.Services.AddQuartzHostedService(opt =>
+{
+    opt.WaitForJobComplete = true;
+});
 
 var app = builder.Build();
 
