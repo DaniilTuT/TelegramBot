a99678991c7327cebde488cb4087e6a316a6f52a
Даниил <dgandapas1@gmail.com>
2024-06-01 09:05:21 +0300

    add quartz

---
+++ Infrastructure/Jobs/PersonFindBirthdaysJob.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Repositories;
+
+namespace Infrastructure1.Jobs;
+
+public class PersonFindBirthdaysJob : IJob
+{
+    private readonly IPersonRepository _personRepository;
+
+    public PersonFindBirthdaysJob(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+    
+    public Task Execute(IJobExecuteContext context)
+    { ///TODO: MAIN SUKA Сделать логику получения человека у которого сегодня в текущий день  день рождения и вывод на консолб
+      
+        var persons = _personRepository.GetAll();
+        foreach (var person in  persons)
+        {
+            
+        }
+    }
+}
